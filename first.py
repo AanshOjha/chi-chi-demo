68bacabc6d047304483f2b2bce2e625b9d9154b4
trainer-aj <ananay.wfhintellify@gmail.com>
2024-03-15 01:49:44 +0530

    save

---
+++ first.py
@@ -0,0 +1,25 @@
+# Basics 
+# -------------- DATA TYPES --------------
+# IMAGES = JPEG 
+# VIDEOS = MP4
+# AUDIO = MP3
+
+# String = that can be calculated aka alphabets
+
+name = "string"
+numbers = 10
+float = 3.14
+list = [1, 2, 3, 4, "Chi-chi"]
+dictionary = {"name": "Chi-chi", "age": 20}
+print(f'Printing variable type for {name} => {type(name)}')
+
+# Function : What to perform a task 
+def add(a, b):
+    print(f' After adding {a,b} result is {a + b}')
+
+def square(a):
+    print(f'Square result is {a * a}') 
+
+add(1, 2)
+square(2)
+
